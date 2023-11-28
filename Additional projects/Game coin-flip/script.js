4f9da8ef9dc29cbf761ddb32e7715e8a2acf2ee6
GrigoryevaEva <grigoryeva.eva.2704@mail.ru>
2023-11-28 17:36:46 +0300

    coin-flip

--- Additional projects/Game coin-flip/script.js
+++ Additional projects/Game coin-flip/script.js
@@ -3,8 +3,7 @@
 function gameCoinFlip (guess) {
   let randomNumber = Math.random();
   let resultToos = randomNumber < 0.5 ? "heads" : "tails";
-  let result;
-  return result = guess === resultToos ? console.log('You win!') : console.log('You lose!');
+  return guess === resultToos ? console.log('You win!') : console.log('You lose!');
 };
 
 
